5d6b5418b07f5771474449ef6c58f4f8242b737f
Vaszil Ádám <vaszil.adam@fps.hu>
2017-03-15 15:18:41 +0100

    Added checklist function to the checkboxes

--- src/main/java/flekken/releasechecklist/ui/ToolWindow.java
+++ src/main/java/flekken/releasechecklist/ui/ToolWindow.java
@@ -8,27 +8,32 @@ import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by on 2017.03.11..
  */
 public class ToolWindow implements ToolWindowFactory {
 
-    private JButton newRelease;
-    private JCheckBox devUpToDate;
-    private JCheckBox masterUpToDate;
-    private JCheckBox mergeDevToMaster;
-    private JCheckBox onMaster;
-    private JCheckBox increaseVersion;
-    private JCheckBox commitVersion;
-    private JCheckBox tagCommit;
-    private JCheckBox buildRelease;
-    private JCheckBox pushToMaster;
-    private JCheckBox mergeToDevelop;
-    private JCheckBox pushToDevelop;
+    private static final int POSITION_DEV_UP_TO_DATE = 0;
+    private static final int POSITION_MASTER_UP_TO_DATE = 1;
+    private static final int POSITION_MERGED_DEV_TO_MASTER = 2;
+    private static final int POSITION_ON_MASTER = 3;
+    private static final int POSITION_INCREASE_VERSION = 4;
+    private static final int POSITION_COMMIT_VERSION = 5;
+    private static final int POSITION_TAG_COMMIT = 6;
+    private static final int POSITION_BUILD_RELEASE = 7;
+    private static final int POSITION_PUSH_MASTER = 8;
+    private static final int POSITION_MERGE_MASTER_TO_DEV = 9;
+    private static final int POSITION_PUSH_TO_DEV = 10;
+
+    private List<JCheckBox> checkBoxList;
 
     /**
      * This method is used to initialize the ui.
+     * Called once when first opening the toolWindow
      */
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull com.intellij.openapi.wm.ToolWindow toolWindow) {
@@ -52,22 +57,102 @@ public class ToolWindow implements ToolWindowFactory {
         constraints.gridwidth = 1;
         constraints.gridheight = 1;
 
-        newRelease = new JButton("New release");
+        JButton newRelease = new JButton("New release");
         constraints.gridy = 0;
         constraints.fill = GridBagConstraints.HORIZONTAL;
+        newRelease.addActionListener(e -> {
+            for (int i = 0, checkBoxListSize = checkBoxList.size(); i < checkBoxListSize; i++) {
+                JCheckBox checkBox = checkBoxList.get(i);
+                if (i == POSITION_DEV_UP_TO_DATE) {
+                    checkBox.setEnabled(true);
+                } else {
+                    checkBox.setEnabled(false);
+                }
+                checkBox.setSelected(false);
+            }
+        });
         panel.add(newRelease, constraints);
 
-        devUpToDate = createCheckBox(panel, constraints, "Develop branch up-to-date", 1);
-        masterUpToDate = createCheckBox(panel, constraints, "Master branch up-to-date", 2);
-        mergeDevToMaster = createCheckBox(panel, constraints, "Merge develop to master", 3);
-        onMaster = createCheckBox(panel, constraints, "On master", 4);
-        increaseVersion = createCheckBox(panel, constraints, "Set version name and code", 5);
-        commitVersion = createCheckBox(panel, constraints, "Commit new version", 6);
-        tagCommit = createCheckBox(panel, constraints, "Build release", 7);
-        buildRelease = createCheckBox(panel, constraints, "Push changes to master", 8);
-        pushToMaster = createCheckBox(panel, constraints, "Push changes to master", 9);
-        mergeToDevelop = createCheckBox(panel, constraints, "Merge master to develop", 10);
-        pushToDevelop = createCheckBox(panel, constraints, "Push changes to develop", 11);
+        checkBoxList = new ArrayList<>(11);
+
+        CheckBoxBuilder checkBoxBuilder = new CheckBoxBuilder(panel, constraints);
+
+        checkBoxList.add(checkBoxBuilder
+                .row(checkBoxList.size() + 1)
+                .label("Develop branch up-to-date")
+                .enabled(true)
+                .listener(l -> setCheckboxesState(POSITION_DEV_UP_TO_DATE))
+                .build());
+
+        checkBoxList.add(checkBoxBuilder
+                .row(checkBoxList.size() + 1)
+                .label("Master branch up-to-date")
+                .enabled(false)
+                .listener(l -> setCheckboxesState(POSITION_MASTER_UP_TO_DATE))
+                .build());
+
+        checkBoxList.add(checkBoxBuilder
+                .row(checkBoxList.size() + 1)
+                .label("Merge develop to master")
+                .enabled(false)
+                .listener(l -> setCheckboxesState(POSITION_MERGED_DEV_TO_MASTER))
+                .build());
+
+        checkBoxList.add(checkBoxBuilder
+                .row(checkBoxList.size() + 1)
+                .label("On master")
+                .enabled(false)
+                .listener(l -> setCheckboxesState(POSITION_ON_MASTER))
+                .build());
+
+        checkBoxList.add(checkBoxBuilder
+                .row(checkBoxList.size() + 1)
+                .label("Set version name and code")
+                .enabled(false)
+                .listener(l -> setCheckboxesState(POSITION_INCREASE_VERSION))
+                .build());
+
+        checkBoxList.add(checkBoxBuilder
+                .row(checkBoxList.size() + 1)
+                .label("Commit new version")
+                .enabled(false)
+                .listener(l -> setCheckboxesState(POSITION_COMMIT_VERSION))
+                .build());
+
+        checkBoxList.add(checkBoxBuilder
+                .row(checkBoxList.size() + 1)
+                .label("Tag commit")
+                .enabled(false)
+                .listener(l -> setCheckboxesState(POSITION_TAG_COMMIT))
+                .build());
+
+        checkBoxList.add(checkBoxBuilder
+                .row(checkBoxList.size() + 1)
+                .label("Build release")
+                .enabled(false)
+                .listener(l -> setCheckboxesState(POSITION_BUILD_RELEASE))
+                .build());
+
+        checkBoxList.add(checkBoxBuilder
+                .row(checkBoxList.size() + 1)
+                .label("Push changes to master")
+                .enabled(false)
+                .listener(l -> setCheckboxesState(POSITION_PUSH_MASTER))
+                .build());
+
+        checkBoxList.add(checkBoxBuilder
+                .row(checkBoxList.size() + 1)
+                .label("Merge master to develop")
+                .enabled(false)
+                .listener(l -> setCheckboxesState(POSITION_MERGE_MASTER_TO_DEV))
+                .build());
+
+        checkBoxList.add(checkBoxBuilder
+                .row(checkBoxList.size() + 1)
+                .label("Push changes to develop")
+                .enabled(false)
+                .listener(null)
+                .build());
 
         //Push components to the top left corner
         Component horizontalSpace = Box.createHorizontalGlue();
@@ -93,17 +178,24 @@ public class ToolWindow implements ToolWindowFactory {
         return scrollPane;
     }
 
-    private JCheckBox createCheckBox(JPanel panel, GridBagConstraints constraints, String label, int row) {
-        JCheckBox checkbox = new JCheckBox(label);
-        constraints.fill = GridBagConstraints.NONE;
-        constraints.gridy = row;
-        panel.add(checkbox, constraints);
-        return checkbox;
+    private void setCheckboxesState(int currentPosition) {
+        if (checkBoxList.get(currentPosition).isSelected()) {
+            checkBoxList.get(currentPosition + 1).setEnabled(true);
+        } else {
+            for (int i = currentPosition + 1, checkBoxListSize = checkBoxList.size(); i < checkBoxListSize; i++) {
+                JCheckBox checkBox = checkBoxList.get(i);
+                checkBox.setSelected(false);
+                checkBox.setEnabled(false);
+            }
+        }
     }
 
+    /**
+     * Called when project opens and plugin is initialized
+     */
     @Override
     public void init(com.intellij.openapi.wm.ToolWindow window) {
-
+        System.out.println("init");
     }
 
     @Override
@@ -114,5 +206,53 @@ public class ToolWindow implements ToolWindowFactory {
     @Override
     public boolean isDoNotActivateOnStart() {
         return true;
+    }
+
+
+    private static class CheckBoxBuilder {
+
+        public final JPanel panel;
+        public final GridBagConstraints constraints;
+        private int row;
+        private boolean isEnabled = false;
+        private String label;
+        private ActionListener listener;
+
+        public CheckBoxBuilder(JPanel panel, GridBagConstraints constraints) {
+            this.panel = panel;
+            this.constraints = constraints;
+        }
+
+        public CheckBoxBuilder row(int val) {
+            this.row = val;
+            return this;
+        }
+
+        public CheckBoxBuilder enabled(boolean val) {
+            this.isEnabled = val;
+            return this;
+        }
+
+        public CheckBoxBuilder label(String val) {
+            this.label = val;
+            return this;
+        }
+
+        public CheckBoxBuilder listener(ActionListener val) {
+            this.listener = val;
+            return this;
+        }
+
+        public JCheckBox build() {
+            JCheckBox checkbox = new JCheckBox(label);
+            constraints.fill = GridBagConstraints.NONE;
+            constraints.gridy = row;
+            checkbox.setEnabled(isEnabled);
+            if (listener != null) {
+                checkbox.addActionListener(listener);
+            }
+            panel.add(checkbox, constraints);
+            return checkbox;
+        }
     }
 }
